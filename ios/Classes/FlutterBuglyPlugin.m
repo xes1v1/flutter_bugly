3a3e37424fdaa49d8f8027263527823f6c65353d
王帅 <wangshuai14@100tal.com>
2020-05-26 15:37:26 +0800

    去掉iOS插件内部bugly的初始化，统一放到业务侧

--- ios/Classes/FlutterBuglyPlugin.m
+++ ios/Classes/FlutterBuglyPlugin.m
@@ -16,15 +16,15 @@
       NSString *appId = call.arguments[@"appId"];
       BOOL b = [self isBlankString:appId];
       if(!b){
-          BuglyConfig * config = [[BuglyConfig alloc] init];
-          NSString *channel = call.arguments[@"channel"];
-          BOOL isChannelEmpty = [self isBlankString:channel];
-          if(!isChannelEmpty){
-            config.channel = channel;
-          }
-          [Bugly startWithAppId:appId config:config];
-          NSLog(@"Bugly appId: %@", appId);
-          [DReportConfig configWithAppID:@"1002667" appKey:@"dad4455adf78b7c92ca1f6cd2d0c9001"];
+//          BuglyConfig * config = [[BuglyConfig alloc] init];
+//          NSString *channel = call.arguments[@"channel"];
+//          BOOL isChannelEmpty = [self isBlankString:channel];
+//          if(!isChannelEmpty){
+//            config.channel = channel;
+//          }
+//          [Bugly startWithAppId:appId config:config];
+//          NSLog(@"Bugly appId: %@", appId);
+//          [DReportConfig configWithAppID:@"1002667" appKey:@"dad4455adf78b7c92ca1f6cd2d0c9001"];
 
           NSDictionary * dict = @{@"message":@"Bugly 初始化成功",@"appId":appId, @"isSuccess":@YES};
           NSData * jsonData = [NSJSONSerialization dataWithJSONObject:dict options:NSJSONWritingPrettyPrinted error:nil];
